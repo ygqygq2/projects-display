468b43cc8e66b4b2080f4abd7140a6d5a6ed55d9
ygqygq2 <ygqygq2@qq.com>
2023-07-25 18:54:40 +0800

    feat: 增加 content 组件
    Co-authored-by: ygqygq2 <29ygq@sina.com>

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -7,22 +7,27 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: '首页', link: '/' },
+      // { text: 'Examples', link: '/markdown-examples' }
     ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
+    // sidebar: [
+    //   {
+    //     text: 'Examples',
+    //     items: [
+    //       { text: 'Markdown Examples', link: '/markdown-examples' },
+    //       { text: 'Runtime API Examples', link: '/api-examples' }
+    //     ]
+    //   }
+    // ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
+      { icon: 'github', link: 'https://github.com/ygqygq2' }
+    ],
+
+    footer: {
+      message: '个人网站',
+      copyright: 'Copyright © 2023-present Created by ygqygq2'
+    }
   }
 })
--- index.md
+++ index.md
@@ -2,23 +2,20 @@
 # https://vitepress.dev/reference/default-theme-home-page
 layout: home
 
-hero:
-  name: "项目展示"
-  text: ""
-  tagline: 全栈项目展示
-  actions:
-    - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
-    - theme: alt
-      text: API Examples
-      link: /api-examples
+# hero:
+#   name: "项目展示"
+#   text: ""
+#   tagline: 全栈项目展示
+#   actions:
+#     - theme: brand
+#       text: Markdown Examples
+#       link: /markdown-examples
+#     - theme: alt
+#       text: API Examples
+#       link: /api-examples
 
-features:
-  - title: 问卷调查
-    details: 一个基于问卷组件的调查问卷系统
-  - title: 项目 B
-    details: xxxx
-  - title: 项目 C
-    details: xxxx
 ---
+<script setup>
+import Content from './.vitepress/components/Content.vue'
+</script>
+<Content />
