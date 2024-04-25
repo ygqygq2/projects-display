66df4473427d9d581fa8be436e4a07521ba79d47
ygqygq2 <ygqygq2@qq.com>
2024-04-25 15:21:11 +0800

    fix: 首页链接居中问题
    Co-authored-by: ygqygq2 <ygqygq2@qq.com>

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '学习无止尽跟上步伐';
+export const SITE_TITLE = '全栈学习之路';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
